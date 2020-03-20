a99a8b34c5ca2238d37649150a17068e0a85e2f4
Jomage <>
2020-03-20 12:23:14 +0100

    step 11 - Add RecyclerView

--- app/src/main/java/com/example/androidroom/MainActivity.kt
+++ app/src/main/java/com/example/androidroom/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.androidroom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.androidroom.recyclerview.WordListAdapter
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = WordListAdapter(this)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
