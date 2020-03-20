e130133632667599d976b19bb747a9c8c2c57896
Jomage <>
2020-03-20 11:18:05 +0100

    step5 - Create Dao

---
+++ app/src/main/java/com/example/androidroom/WordDao.kt
@@ -0,0 +1,19 @@
+package com.example.androidroom
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface WordDao {
+
+    @Query("SELECT * from word_table ORDER BY word asc")
+    fun getAlphabetizedWords(): List<Word>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(word: Word)
+
+    @Query("DELETE from word_table")
+    suspend fun deleteAll()
+}
